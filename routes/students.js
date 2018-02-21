84afc1747a613ccdb94b6ab82d365158d8efce14
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-02-22 00:20:57 +0100

    Add route for randomizer

--- routes/students.js
+++ routes/students.js
@@ -29,6 +29,40 @@ module.exports = io => {
         })
         .catch((error) => next(error))
     })
+
+    .get('/students/batch/:id/random', (req, res, next) => {
+      const id = req.params.id
+
+      Student.find({ batchNum : id }) // A SYNCHROON
+        .then((studentList) => {
+          const redStudents = studentList.filter(student => student.red)
+          const yellowStudents = studentList.filter(student => student.yellow)
+          const greenStudents = studentList.filter(student => student.green)
+
+          const randomRed = redStudents[Math.floor(Math.random()*redStudents.length)]
+          const randomYellow = yellowStudents[Math.floor(Math.random()*yellowStudents.length)]
+          const randomGreen = greenStudents[Math.floor(Math.random()*greenStudents.length)]
+
+          const randomStudent = Math.random() * 100
+
+          if (randomStudent < 18) {
+              student = randomGreen
+            } else if (randomStudent < 51) {
+                student = randomYellow
+            } else {
+              student = randomRed
+            }
+
+            if(student === undefined) {
+              student = studentList[Math.floor(Math.random()*studentList.length)]
+            }
+
+          res.json(student)
+        })
+        .catch((error) => next(error))
+    })
+
+
     .post('/students', authenticate, (req, res, next) => {
       const newStudent = req.body
 
