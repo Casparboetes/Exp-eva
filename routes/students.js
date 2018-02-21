36d2a381f2906ae1bcc63378ecf6a835f92ad330
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-02-21 09:01:59 +0100

    Add algorithm and a test file

--- routes/students.js
+++ routes/students.js
@@ -3,6 +3,8 @@ const router = require('express').Router()
 const passport = require('../config/auth')
 const { Student } = require('../models')
 const processMove = require('../lib/processMove')
+// const tesktRandomizer = require('../lib/tesktRandomizer')
+
 
 const authenticate = passport.authorize('jwt', { session: false })
 
